23fc410545be49616e195fd221c891957a438f15
gakeerdeng03 <gakeerdeng03@gmail.com>
2026-05-14 05:04:47 +0300

    Create else if .cpp

---
+++ else if .cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){ 
+     int score{100}; 
+    if (score >= 90){
+        cout << "grade A \n:";}
+        else if (score >=80){
+            cout << "grade B \n:";}
+            else 
+               {cout << "grade F \n:"; }
+        
+    
+return 0;}
